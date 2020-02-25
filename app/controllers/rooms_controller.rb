c9874e3e769f8a833818e4aaa2fb6cc7f5ceb0e0
kento.kuroda <kent.ulti91@gmail.com>
2020-02-25 13:53:21 +0000

    henkou

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,7 @@ class RoomsController < ApplicationController
 
   def show
     @room = Room.find(params[:id])
-    if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @messages = @room.messages
       @message = Message.new
       @entries = @room.entries.where('user_id != ?', current_user.id)
